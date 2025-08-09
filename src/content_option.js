43b633103928a61c81e26c10ff3fa1910f6b92bf
RichardBooker19951230 <RichardBooker19951230@gmail.com>
2025-08-09 15:10:21 -0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,224 @@
+const logotext = "Richard";
+const meta = {
+    title: "Richard Booker",
+    description: "I’m a Senior Full-Stack Devloper",
+};
+
+const introdata = {
+    title: "I’m Richard Booker",
+    animated: {
+        first: "I am a senior full-stack developer and I can carry out any task.",
+        second: "I love coding and I can code cool websites.",
+        third: "I can also develop Mobile apps and Blockchain.",
+    },
+    description: "I am an experienced Senior Full-Stack Developer with a strong foundation in JavaScript, TypeScript and modern webframeworks like React, Next.js, Angular, Vue, and Svelte. I specialize in building scalable back-end solutions withNode.js, Express, and cloud services such as AWS",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "  I'm an experienced Full-Stack Developer with a strong foundation in JavaScript, TypeScript, \
+                and modern webframeworks like React, Next.js, Angular, Vue, and Svelte. I specialize in \
+                building scalable back-end solutions withNode.js, Express, and cloud services such as AWS \
+                Lambda and Firebase.Beyond coding, I focus on improving performance, streamlining CI/CD \
+                workflows, and enhancing user experiencesthrough thoughtful UI development and state \
+                management. I have a deep understanding of microservicesarchitecture, RESTful and \
+                GraphQL API integrations, and serverless computing.Iʼm passionate about writing clean, \
+                maintainable code, mentoring teams, and driving agile developmentpractices. Whether itʼs \
+                refining an application for speed and scalability or leading a team through \
+                complexmigrations, I thrive on solving problems and delivering high-quality software that \
+                makes an impact. I can carry out any task assinged me without hesitation quilkly.",
+};
+
+const skills = [
+    {
+        name: "C",
+        value: 96,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "React",
+        value: 99,
+    },
+    {
+        name: "Angular",
+        value: 96,
+    },
+    {
+        name: "Vue",
+        value: 85,
+    },
+    {
+        name: "MongoDB",
+        value: 97,
+    },
+    {
+        name: "MySQL",
+        value: 95,
+    },
+    {
+        name: "BlockChain",
+        value: 85,
+    },
+    {
+        name: "Android",
+        value: 89,
+    },
+];
+
+const services = [{
+        title: "Web Development",
+        description: "I can develop any kind of Web site quikly and greatly.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "I can make clean UI design and well-documented cording.",
+    },
+    {
+        title: "Blockchain",
+        description: "I can ensure the quality of project.",
+    },
+];
+
+const worktimeline = [
+    {
+        jobtitle: "Caroom",
+        where: "United States",
+        date: "2025",
+    },
+    {
+        jobtitle: "PokéSG",
+        where: "Singapore",
+        date: "2024",
+    },
+    {
+        jobtitle: "Crocs™ Singapore",
+        where: "Singapore",
+        date: "2023",
+    },
+    {
+        jobtitle: "Auto Collision Repair Shop",
+        where: "United States",
+        date: "2022",
+    },
+    {
+        jobtitle: "Your Best Address",
+        where: "United States",
+        date: "2021",
+    },
+    {
+        jobtitle: "Phillipsburg NJ",
+        where: "United States",
+        date: "2020",
+    },
+    {
+        jobtitle: "Affordable Bus Tickets and Bus Schedules",
+        where: "United States",
+        date: "2019",
+    },
+    {
+        jobtitle: "Buy Posters & Frames",
+        where: "Canada",
+        date: "2018",
+    },
+    {
+        jobtitle: "Express Auto Shop",
+        where: "United States",
+        date: "2017",
+    },
+    {
+        jobtitle: "Hot & Cold Water Dispensers",
+        where: "Singapore",
+        date: "2016",
+    },
+    {
+        jobtitle: "INTEQUA DESIGNS",
+        where: "United States",
+        date: "2015",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "./images/caroom2.png",
+        description: "Sell Your Car - Get the Best Offer for Your Car | Caroom",
+        link: "https://caroom.us/",
+    },
+    {
+        img: "./images/pokesg.png",
+        description: "Online Store For Pokémon Trading Card Game Products | PokéSG",
+        link: "https://pokesg.com/",
+    },
+    {
+        img: "./images/heritage.png",
+        description: "Auto Collision Repair Shop Austin and Cedar Park",
+        link: "http://heritagebodyandframe.com/",
+    },
+    {
+        img: "./images/express2.png",
+        description: "The Best And #1 Auto Repair In Irving-TX - Express Auto Shop",
+        link: "http://expressautoirvingtx.com/",
+    },
+    {
+        img: "./images/yourbestaddress.png",
+        description: "Your Best Address | Mail Forwarding & South Dakota Residency Experts - Your Best Address",
+        link: "http://yourbestaddress.com/",
+    },
+    {
+        img: "./images/massapothecary.png",
+        description: "Flooring Store servicing Eustis, FL | Floors & More",
+        link: "http://massapothecary.com/",
+    },
+    {
+        img: "./images/trailways2.png",
+        description: "Affordable Bus Tickets and Bus Schedules",
+        link: "http://trailways.com/",
+    },
+    {
+        img: "./images/puricare2.png",
+        description: "Hot & Cold Water Dispensers Singapore | Offices & Homes | Puricare",
+        link: "http://puricare.com.sg/",
+    },
+    {
+        img: "./images/intequa.png",
+        description: "Architectural Design Firm | INTEQUA DESIGNS",
+        link: "http://intequa.ca/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "richardbooker@gmail.com",
+    YOUR_FONE: "(640)277-0332",
+    description: "I hope we will collarborate in future.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
